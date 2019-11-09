52647e6c4fef4e51c21377e5bc12611564a9b315
chudomozg <chudomozg@gmail.com>
2019-11-09 22:27:40 +1000

    add scripts
    add: webpack, babel, react, redux,

---
+++ src/index.js
@@ -0,0 +1,12 @@
+"use strict";
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App.js";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
